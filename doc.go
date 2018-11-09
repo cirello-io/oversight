3c26b8588e82ff90322670f836b3a4316c043dfe
Ulderico Cirello <u@cirello.eu>
2018-11-09 15:50:06 +0100

    oversight: add package level documentation

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package oversight makes a nearly complete implementation of the Erlang
+// supervision trees.
+//
+// Refer to: http://erlang.org/doc/design_principles/sup_princ.html
+package oversight // import "cirello.io/oversight"
