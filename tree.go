cb14dc2e4c7646026cb351df0f3f01e4f81388bb
Ulderico Cirello <u@cirello.eu>
2018-11-14 17:21:45 +0100

    oversight: code grooming

--- tree.go
+++ tree.go
@@ -15,6 +15,12 @@ var ErrTooManyFailures = errors.New("too many failures")
 // and there is no one left to restart.
 var ErrNoChildProcessLeft = errors.New("no child process left")
 
+// Oversight creates and ignites a supervisor tree.
+func Oversight(opts ...Option) ChildProcess {
+	t := New(opts...)
+	return t.Start
+}
+
 // Tree is the supervisor tree proper.
 type Tree struct {
 	initializeOnce sync.Once
@@ -53,12 +59,6 @@ func (t *Tree) err() error {
 	return t.rootErr
 }
 
-// Oversight creates and ignites a supervisor tree.
-func Oversight(opts ...Option) ChildProcess {
-	t := New(opts...)
-	return t.Start
-}
-
 func (t *Tree) init() {
 	t.initializeOnce.Do(func() {
 		if t.maxR == 0 && t.maxT == 0 {
@@ -84,7 +84,6 @@ func (t *Tree) Start(rootCtx context.Context) error {
 	failure := make(chan int)
 	t.states = make([]state, len(t.processes))
 	wg.Add(1)
-	// TODO(uc): refactor code to use just one loop instead of two.
 	go func() {
 		defer wg.Done()
 		for {
@@ -92,54 +91,51 @@ func (t *Tree) Start(rootCtx context.Context) error {
 			case <-ctx.Done():
 				return
 			default:
-				anyStartedProcess := false
-				startSemaphore := make(chan struct{})
-				for i, p := range t.processes {
-					running := t.states[i].current()
-					if running.state == "running" {
-						anyStartedProcess = true
-						continue
-					}
-					if running.state == "done" {
-						continue
-					}
-					if running.state == "failed" && !p.restart(running.err) {
-						continue
-					}
-					anyStartedProcess = true
-					childCtx, childCancel := context.WithCancel(ctx)
-					var childWg sync.WaitGroup
-					wg.Add(1)
-					childWg.Add(1)
-					t.states[i].setRunning(func() {
-						childCancel()
-						childWg.Wait()
-					})
-					go func(i int, p childProcess) {
-						defer wg.Done()
-						defer childWg.Done()
-						<-startSemaphore
-						err := safeRun(childCtx, p.f)
-						restart := p.restart(err)
-						t.states[i].setErr(err, restart)
-						// TODO(uc): add support for timeout detach
-						select {
-						case <-childCtx.Done():
-						case failure <- i:
-						}
+			}
 
-					}(i, p)
+			anyStartedProcess := false
+			startSemaphore := make(chan struct{})
+			for i, p := range t.processes {
+				running := t.states[i].current()
+				if running.state == "running" {
+					anyStartedProcess = true
+					continue
 				}
-				close(startSemaphore)
-				if !anyStartedProcess {
-					t.setErr(ErrNoChildProcessLeft)
-					cancel()
-					return
+				if running.state == "done" {
+					continue
 				}
-				select {
-				case <-ctx.Done():
-				default:
+				if running.state == "failed" && !p.restart(running.err) {
+					continue
 				}
+				anyStartedProcess = true
+				childCtx, childCancel := context.WithCancel(ctx)
+				var childWg sync.WaitGroup
+				wg.Add(1)
+				childWg.Add(1)
+				t.states[i].setRunning(func() {
+					childCancel()
+					childWg.Wait()
+				})
+				go func(i int, p childProcess) {
+					defer wg.Done()
+					defer childWg.Done()
+					<-startSemaphore
+					err := safeRun(childCtx, p.f)
+					restart := p.restart(err)
+					t.states[i].setErr(err, restart)
+					// TODO(uc): add support for timeout detach
+					select {
+					case <-childCtx.Done():
+					case failure <- i:
+					}
+
+				}(i, p)
+			}
+			close(startSemaphore)
+			if !anyStartedProcess {
+				t.setErr(ErrNoChildProcessLeft)
+				cancel()
+				return
 			}
 
 			select {
@@ -158,74 +154,4 @@ func (t *Tree) Start(rootCtx context.Context) error {
 
 	wg.Wait()
 	return t.err()
-}
-
-// Option are applied to change the behavior of a Tree.
-type Option func(*Tree)
-
-// WithSpecification defines a custom setup to tweak restart tolerance and
-// strategy for the instance of oversight.
-func WithSpecification(maxR int, maxT time.Duration, strategy Strategy) Option {
-	return func(t *Tree) {
-		WithRestartIntensity(maxR, maxT)(t)
-		WithRestartStrategy(strategy)(t)
-	}
-}
-
-// WithRestartIntensity defines a custom tolerance for failures in the
-// supervisor tree.
-func WithRestartIntensity(maxR int, maxT time.Duration) Option {
-	return func(t *Tree) {
-		t.maxR, t.maxT = maxR, maxT
-	}
-}
-
-// Default restart intensity expectations.
-const (
-	DefaultMaxR = 1
-	DefaultMaxT = 5 * time.Second
-)
-
-// DefaultRestartIntensity redefines the tolerance for failures in the
-// supervisor tree. It defaults to 1 restart (maxR) in the preceding 5 seconds
-// (maxT).
-func DefaultRestartIntensity(t *Tree) {
-	t.maxR, t.maxT = DefaultMaxR, DefaultMaxT
-}
-
-// WithRestartStrategy defines a custom restart strategy for the supervisor
-// tree.
-func WithRestartStrategy(strategy Strategy) Option {
-	return func(t *Tree) {
-		t.strategy = strategy
-	}
-}
-
-// DefaultRestartStrategy redefines the supervisor behavior to use OneForOne.
-func DefaultRestartStrategy(t *Tree) {
-	t.strategy = OneForOne
-}
-
-// Processes plugs one or more Permanent child processes to the supervisor tree.
-// Processes never reset the child process list.
-func Processes(processes ...ChildProcess) Option {
-	return func(t *Tree) {
-		for _, p := range processes {
-			t.processes = append(t.processes, childProcess{
-				restart: Permanent,
-				f:       p,
-			})
-		}
-	}
-}
-
-// Process plugs one child processes to the supervisor tree. Process never reset
-// the child process list.
-func Process(restart Restart, process ChildProcess) Option {
-	return func(t *Tree) {
-		t.processes = append(t.processes, childProcess{
-			restart: restart,
-			f:       process,
-		})
-	}
 }
---
+++ tree_options.go
@@ -0,0 +1,73 @@
+package oversight
+
+import "time"
+
+// Option are applied to change the behavior of a Tree.
+type Option func(*Tree)
+
+// WithSpecification defines a custom setup to tweak restart tolerance and
+// strategy for the instance of oversight.
+func WithSpecification(maxR int, maxT time.Duration, strategy Strategy) Option {
+	return func(t *Tree) {
+		WithRestartIntensity(maxR, maxT)(t)
+		WithRestartStrategy(strategy)(t)
+	}
+}
+
+// WithRestartIntensity defines a custom tolerance for failures in the
+// supervisor tree.
+func WithRestartIntensity(maxR int, maxT time.Duration) Option {
+	return func(t *Tree) {
+		t.maxR, t.maxT = maxR, maxT
+	}
+}
+
+// Default restart intensity expectations.
+const (
+	DefaultMaxR = 1
+	DefaultMaxT = 5 * time.Second
+)
+
+// DefaultRestartIntensity redefines the tolerance for failures in the
+// supervisor tree. It defaults to 1 restart (maxR) in the preceding 5 seconds
+// (maxT).
+func DefaultRestartIntensity(t *Tree) {
+	t.maxR, t.maxT = DefaultMaxR, DefaultMaxT
+}
+
+// WithRestartStrategy defines a custom restart strategy for the supervisor
+// tree.
+func WithRestartStrategy(strategy Strategy) Option {
+	return func(t *Tree) {
+		t.strategy = strategy
+	}
+}
+
+// DefaultRestartStrategy redefines the supervisor behavior to use OneForOne.
+func DefaultRestartStrategy(t *Tree) {
+	t.strategy = OneForOne
+}
+
+// Processes plugs one or more Permanent child processes to the supervisor tree.
+// Processes never reset the child process list.
+func Processes(processes ...ChildProcess) Option {
+	return func(t *Tree) {
+		for _, p := range processes {
+			t.processes = append(t.processes, childProcess{
+				restart: Permanent,
+				f:       p,
+			})
+		}
+	}
+}
+
+// Process plugs one child processes to the supervisor tree. Process never reset
+// the child process list.
+func Process(restart Restart, process ChildProcess) Option {
+	return func(t *Tree) {
+		t.processes = append(t.processes, childProcess{
+			restart: restart,
+			f:       process,
+		})
+	}
+}
