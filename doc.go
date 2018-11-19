66406fc9bcf8e1891d79effc3501c3e6cc7f2ed9
Ulderico Cirello <u@cirello.eu>
2018-11-19 16:42:20 +0100

    oversight: add quickstart to package level documentation

--- doc.go
+++ doc.go
@@ -2,4 +2,21 @@
 // supervision trees.
 //
 // Refer to: http://erlang.org/doc/design_principles/sup_princ.html
+//
+//     supervise := oversight.Oversight(
+//     	oversight.Processes(func(ctx context.Context) error {
+//     		select {
+//     		case <-ctx.Done():
+//     			return nil
+//     		case <-time.After(time.Second):
+//     			log.Println(1)
+//     		}
+//     		return nil
+//     	}),
+//     )
+//     ctx, cancel := context.WithCancel(context.Background())
+//     defer cancel()
+//     if err := supervise(ctx); err != nil {
+//     	log.Fatal(err)
+//     }
 package oversight // import "cirello.io/oversight"
