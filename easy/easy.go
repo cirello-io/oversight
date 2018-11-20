ef4eeee5df674e1f0d03480ec4d92afb5048a6b0
Ulderico Cirello <u@cirello.eu>
2018-11-20 14:38:24 +0100

    oversight: upgrade license to Apache 2.0

--- easy/easy.go
+++ easy/easy.go
@@ -1,3 +1,17 @@
+// Copyright 2018 cirello.io/oversight - Ulderico Cirello
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /*
 Package easy is an easier interface to use cirello.io/oversight. Its lifecycle
 is managed through context.Context. Stop a given oversight tree by cancelling
