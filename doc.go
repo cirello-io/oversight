f344d9749f6a019e7f7ad8feac149fc1bc2d4f27
Ulderico Cirello <u@cirello.eu>
2019-01-12 21:49:33 +0100

    oversight: expand package level documentation with oversight/easy example

--- doc.go
+++ doc.go
@@ -18,6 +18,7 @@
 // Refer to: http://erlang.org/doc/design_principles/sup_princ.html
 //
 //     supervisor := oversight.New(
+//     	oversight.WithRestartStrategy(oversight.OneForOne()),
 //     	oversight.Processes(func(ctx context.Context) error {
 //     		select {
 //     		case <-ctx.Done():
@@ -33,6 +34,25 @@
 //     if err := supervisor.Start(ctx); err != nil {
 //     	log.Fatal(err)
 //     }
+//
+// Simple interface
+//
+// If you do not need to use nested trees, you might prefer using
+// cirello.io/oversight/easy instead. It provides a OneForAll tree with the
+// automatic halting disabled.
+//
+// 	package main
+//
+// 	import oversight "cirello.io/oversight/easy"
+//
+// 	func main() {
+// 		ctx, cancel := context.WithCancel(context.Background())
+// 		defer cancel() // use cancel() to halt the tree.
+// 		ctx = oversight.WithContext(ctx)
+// 		oversight.Add(ctx, func(ctx context.Context) {
+// 			// ...
+// 		})
+// 	}
 //
 // This package is covered by this SLA: https://cirello.io/sla
 package oversight // import "cirello.io/oversight"
