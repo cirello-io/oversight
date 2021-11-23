2db6228fdf74386bf55bfda1811e72c18d382118
Ulderico Cirello <uldericofilho@gmail.com>
2021-11-23 13:50:52 -0800

    oversight: make Test_dynamicNesting to wait for all goroutines to finish before yielding

--- tree_test.go
+++ tree_test.go
@@ -1086,19 +1086,24 @@ func Test_dynamicNesting(t *testing.T) {
 	t.Parallel()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	var wg sync.WaitGroup
 	tree := oversight.New(oversight.NeverHalt())
 	// The point of this test is to prove that the clockwork won't panic.
 	// The self-deletion of dynamically added children is, as of the time
 	// of this test, unspecified behavior.
+	wg.Add(1)
 	tree.Add(oversight.ChildProcessSpecification{
 		Name:    "root",
 		Restart: oversight.Transient(),
 		Start: func(ctx context.Context) error {
+			defer wg.Done()
 			for i := 0; i < 10; i++ {
+				wg.Add(1)
 				tree.Add(oversight.ChildProcessSpecification{
 					Name:    "child",
 					Restart: oversight.Transient(),
 					Start: func(ctx context.Context) error {
+						defer wg.Done()
 						realChildName := oversight.ChildProcessName(ctx)
 						t.Log("start", realChildName)
 						defer t.Log("done", realChildName)
@@ -1118,6 +1123,7 @@ func Test_dynamicNesting(t *testing.T) {
 		},
 	})
 	err := tree.Start(ctx)
+	wg.Wait()
 	if err != nil && err != oversight.ErrNoChildProcessLeft {
 		t.Fatal("unexpected error:", err)
 	}
