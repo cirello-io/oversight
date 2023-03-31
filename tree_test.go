678eccfbf3a08a73a9dc237eb1049ff6189cc65e
Ulderico Cirello <uldericofilho@gmail.com>
2023-03-31 10:32:26 -0700

    oversight: deflake test mean to prove the clockwork will not panic

--- tree_test.go
+++ tree_test.go
@@ -20,6 +20,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"reflect"
 	"strings"
 	"sync"
@@ -880,7 +881,7 @@ func Test_dynamicNesting(t *testing.T) {
 			for i := 0; i < 10; i++ {
 				wg.Add(1)
 				tree.Add(oversight.ChildProcessSpecification{
-					Name:    "child",
+					Name:    fmt.Sprintf("child %d", rand.Int63()),
 					Restart: oversight.Transient(),
 					Start: func(ctx context.Context) error {
 						defer wg.Done()
@@ -890,13 +891,13 @@ func Test_dynamicNesting(t *testing.T) {
 						select {
 						case <-ctx.Done():
 							return ctx.Err()
-						case <-time.After(1 * time.Second):
+						case <-time.After(100 * time.Millisecond):
 							t.Log("deleting", realChildName)
 							_ = tree.Delete(realChildName)
 							return nil
 						}
 					},
-					Shutdown: oversight.Timeout(1 * time.Second),
+					Shutdown: oversight.Timeout(500 * time.Millisecond),
 				})
 			}
 			return nil
@@ -906,6 +907,9 @@ func Test_dynamicNesting(t *testing.T) {
 	wg.Wait()
 	if err != nil && err != oversight.ErrNoChildProcessLeft {
 		t.Fatal("unexpected error:", err)
+	}
+	if err := tree.Start(context.Background()); !errors.Is(err, oversight.ErrNoChildProcessLeft) {
+		t.Fatal("expected error missing: %$v", err)
 	}
 }
 
